d03a9435ccdaf00c2680cce3a4e30a2bb130ba21
Pavel Gajdusek <gajdusek.pavel@gmail.com>
2020-04-17 18:26:59 +0200

    Updates

---
+++ precache-manifest.bac02e788185088a6290675bf2452cf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9af36efd11d36159df24176804c42d6",
+    "url": "/spotifygraph/index.html"
+  },
+  {
+    "revision": "7cc787af35908f918880",
+    "url": "/spotifygraph/static/css/main.c7e87b54.chunk.css"
+  },
+  {
+    "revision": "0b519761b3977c68f942",
+    "url": "/spotifygraph/static/js/2.74a81d43.chunk.js"
+  },
+  {
+    "revision": "7cc787af35908f918880",
+    "url": "/spotifygraph/static/js/main.e97b97bb.chunk.js"
+  },
+  {
+    "revision": "cffefd6283fd0772a122",
+    "url": "/spotifygraph/static/js/runtime-main.f29cb224.js"
+  }
+]);
